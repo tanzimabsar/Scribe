48de0bacc70e0bd8c9f8498b5d5a05d8f150eac4
Tanzim <tanzim-mbp@tobo.local>
2020-12-29 20:46:22 +0000

    added test db

--- ScribeService/database.py
+++ ScribeService/database.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import configparser
+
+# In production these configs will be places in a secure location and rotated
+config = configparser.ConfigParser()
+config.read("config.ini")
+config = config["dev"]
+
+PG_USER = config["PG_USER"]
+PG_PASSWORD = config["PG_PASSWORD"]
 
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:password@127.0.0.1:5432/scribedev"
+    f"postgresql://{PG_USER}:{PG_PASSWORD}@127.0.0.1:5432/scribedev"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- ScribeService/test_client.py
+++ ScribeService/test_client.py
@@ -1,10 +1,31 @@
 from fastapi.testclient import TestClient
-from .main import app
+from .main import app, get_db
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .database import Base
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
-def test_get_users():
+
+
+def test_create_user():
     """ Assert that a user can successfully create a user profile with user and password """
 
     response = client.post(
@@ -14,3 +35,22 @@ def test_get_users():
     )
 
     assert response.status_code == 200
+
+def test_fail_create_user_with_non_email():
+    pass
+
+def test_fail_if_user_already_registered():
+    """ If a user has been created with the same email address, deny access """
+
+    response = client.post(
+        "/users/",
+        headers={'Content-Type': 'application/json'},
+        json={"email": "test@gmail.com", "password": "password"},
+    )
+
+    assert response.status_code == 200
+
+
+
+
+    
